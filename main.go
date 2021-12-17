19763b6cb4ffece7c692484fba9c62272f1905d0
bernatsc <bernat.segura@students.salle.url.edu>
2021-12-17 19:37:44 +0100

    configs pa heroku

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./json"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	_ = http.ListenAndServe(":3000", nil)
+
+	//Heroku ens diu a quin port
+	port := os.Getenv("PORT")
+	//Si no ens ho especifica triem nosaltres
+	if port == "" {
+		port = "8000"
+	}
+
+	_ = http.ListenAndServe(":"+port, nil)
 }
